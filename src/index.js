64c14f958af465609ef70b7b0f1223913ef10283
RahulNainwal <rahulnainwal7@gmail.com>
2019-09-02 23:47:12 +0530

    new practice using git is added.

--- src/index.js
+++ src/index.js
@@ -8,10 +8,14 @@ import Hooks3 from './functionalComponents/Hooks3'
 import Hooks4 from './functionalComponents/Hooks4'
 import Hooks5 from './functionalComponents/Hooks5'
 import Hooks6 from './functionalComponents/Hooks6'
+import Hooks7 from './functionalComponents/Hooks7'
+import Hooks8 from './functionalComponents/Hooks8'
+import Hooks9 from './functionalComponents/Hooks9'
+import Hooks10 from './functionalComponents/Hooks10'
 import RandomColorGenerator from './functionalComponents/RandomColorGenerator'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<RandomColorGenerator />, document.getElementById('root'));
+ReactDOM.render(<Hooks10 />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
